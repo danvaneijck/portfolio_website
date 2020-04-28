a708323b42554830c3233ca7bb562b5d3a7fbfda
Dan <vaneijdani@myvuw.ac.nz>
2020-04-28 17:26:28 +1200

    website

---
+++ js/projects.js
@@ -0,0 +1,37 @@
+function openPage(pageName, elmnt) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.backgroundColor = "";
+    }
+
+    document.getElementById(pageName).style.display = "block";
+
+    elmnt.style.backgroundColor = "#379683";
+}
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+window.onload = function(){
+    var project = getParameterByName("project");
+    console.log(project);
+    if(project != null){
+        document.getElementById(project + "-btn").click();
+    }else{
+        // Get the element with id="defaultOpen" and click on it
+        document.getElementById("blockchain-btn").click();
+    }
+};
